47fc54435c5b199fcde4b12e754be33223538e6a
Daniel Romero Jimenez <daromero1989@gmail.com>
2024-01-30 23:43:17 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -57,38 +57,7 @@ function setData() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    let boton1 = document.getElementById("buscar");
+    let boton1 = document.getElementById("busca");
     boton1.addEventListener("click", procesarPeticion);
     let boton2 = document.getElementById("limpia");
     boton2.addEventListener("click", limpia);
@@ -100,15 +69,16 @@ function setData() {
 
 
 
-
-
-
-
-
     function procesarPeticion() {
-
+        
         //translateCall
-        let input = document.getElementById("input").value;
+        let input = document.getElementById("input-text").value;
+        
+        if(!input){
+            
+            return;
+        }
+
         let myApiUrl = "https://script.google.com/macros/s/AKfycbw10C-7_mXumBgcYvQ8UwwDkDP4zPA2iMzDxjQC6JqZBHFTVm6nv9SM0hBmy-qJW1Xm5g/exec?text=" + input
 
         $.ajax({
@@ -120,6 +90,7 @@ function setData() {
 
         }).done(function (response) {
             let translatedName = JSON.parse(response).translatedText;
+            
             search(translatedName)
 
         }).fail(processarError);
@@ -128,10 +99,9 @@ function setData() {
 
 
 
-    function search(name) {
-
-
-        var query = name;
+    function search(query) {
+        
+        
         $.ajax({
 
             Zasync: true,
@@ -169,32 +139,30 @@ function setData() {
     }
 
     function renderFound(item) {
+
+        console.log("delete")
         deleteInfo();
-        item.nombre = document.getElementById("input").value;
-        let html = "<h1>" + item.nombre + "(" + item.name + ")</h1>" +
-            "<h3>Composición por 100g</h3>\n" +
+        item.nombre = document.getElementById("input-text").value;
+        let html = `<div class="mx-2 my-1"><h5>` + item.nombre + "(" + item.name + ")</h5>" +
+            `<div class="ltext">`+"<h5>Composición por 100g</h5>\n" +
             "Calorias Totales: " + item.calories + "Kcal<br>" +
             "Proteina: " + item.protein_g + "g<br>" +
             "Carbohidratos: " + item.carbohydrates_total_g + "g<br>" +
-            "&#8627 de los cuales azucares: " + item.sugar_g + "g<br>" +
+            "&nbsp &nbsp &#8627 de los cuales azucares: " + item.sugar_g + "g<br>" +
             "Grasas: " + item.fat_total_g + "g<br>" +
-            "&#8627de las cuales saturadas: " + item.fat_saturated_g + "g<br>" +
-            "&#8627Colesterol: " + item.cholesterol_mg + "mg<br>" +
+            "&nbsp &nbsp &#8627de las cuales saturadas: " + item.fat_saturated_g + "g<br>" +
+            "&nbsp &nbsp &#8627Colesterol: " + item.cholesterol_mg + "mg<br>" +
             "Fibra: " + item.fiber_g + "g<br>" +
             "sodio: " + item.sodium_mg + "mg<br>" +
-            "potasio: " + item.potassium_mg + "mg<br>"
-        let showDiv = document.createElement("div");
-        showDiv.setAttribute("class", "showItem")
+            "potasio: " + item.potassium_mg + "mg<br></p></div>"
+        
 
-        document.getElementById("info").append(showDiv)
-        showDiv.innerHTML = html;
+        document.getElementById("info").innerHTML = html;
     }
 
     function deleteInfo() {
-        let div = document.getElementById("info").firstChild
-        if (div != null) {
-            div.remove();
-        }
+        document.getElementById("info").innerHTML = "";
+        
     }
 
     function limpia() {
@@ -207,10 +175,11 @@ function setData() {
             alert("Antes de añadir es necesario buscar un alimento y tenerlo visualizado");
             return;
         }
-        let cantidad = document.getElementById("inputNumber").value;
+        let cantidad = document.getElementById("input-number").value;
         if (cantidad != null && cantidad > 0) {
             item.cantidad = parseInt(cantidad);
             añadir(item);
+            
         } else {
             alert("los gramos no pueden estar vacios o en 0");
         }
@@ -233,7 +202,7 @@ function setData() {
         <td>${((entrada.sugar_g/100)*entrada.cantidad).toFixed(2)} </td>
         <td>${((entrada.fiber_g/100)*entrada.cantidad).toFixed(2)} </td>
         <td>${((entrada.sodium_mg/100)*entrada.cantidad).toFixed(2)} </td>
-        <td><img src="trash.png" class="trash" onclick="borrar(${entrada.id})"></td> 
+        <td><img src="trash.png" class="trash hover" onclick="borrar(${entrada.id})"></td> 
         
         
         </tr>`;
@@ -270,6 +239,7 @@ function calcTotal() {
 
 
     for (let i in entradas) {
+        console.log(entradas[i])
         totalCantidad += entradas[i].cantidad;
         totalKcal += entradas[i].calories / 100 * entradas[i].cantidad;
         totalProteina += entradas[i].protein_g / 100 * entradas[i].cantidad;
